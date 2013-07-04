b01f5d2b793cf52450c697c22d4f017ece68cf77
Joshua Ball <joshua.g.s.ball@gmail.com>
2013-07-03 17:03:24 -0700

    Fixed google cdnify (bower_components issue). Added deploy dist directory for azure.

---
+++ dist/scripts/afe4c7c3.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("pctApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("pctApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
